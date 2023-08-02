969fb75fb6e6cd9e1ce74f67ca6c8672e0b71c2b
Riya Wagh <123661235+Riyawagh2405@users.noreply.github.com>
2023-08-02 23:35:44 +0530

    dynamic1.c

--- C Programs/dynamic1.c
+++ C Programs/dynamic1.c
@@ -21,16 +21,3 @@ int main()
     
     return 0;
 }
-
-
-
-/*
-
-C:\Users\admin\Desktop\PPA\C Programming>gcc dynamic1.c -o Myexe
-
-C:\Users\admin\Desktop\PPA\C Programming>Myexe
-Enter the size of array
-6
-
-
-*/
