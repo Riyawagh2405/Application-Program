962a6e47fae307376d62541e273537fdb244c3b5
Riya Wagh <123661235+Riyawagh2405@users.noreply.github.com>
2023-08-02 23:37:45 +0530

    iteration1.c

--- C Programs/iteration1.c
+++ C Programs/iteration1.c
@@ -10,17 +10,3 @@ int main()
     
     return 0;
 }
-
-
-/*
-
-C:\Users\admin\Desktop\PPA\C Programming>gcc iteration1.c -o Myexe
-
-C:\Users\admin\Desktop\PPA\C Programming>Myexe
-Jay Ganesh...
-Jay Ganesh...
-Jay Ganesh...
-Jay Ganesh...
-
-
-*/
