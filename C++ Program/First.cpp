9a54c27c61aa8c0d4145eec888607f20e6f1d153
Riya Wagh <123661235+Riyawagh2405@users.noreply.github.com>
2023-08-02 23:51:07 +0530

    First.cpp

--- C++ Program/First.cpp
+++ C++ Program/First.cpp
@@ -8,15 +8,3 @@ int main()
 
     return 0;
 }
-
-
-
-/*
-
-C:\Users\admin\Desktop\PPA\C++Programming>g++ First.cpp -o Myexe
-
-C:\Users\admin\Desktop\PPA\C++Programming>Myexe
-Jay Ganesh...
-
-
-*/
